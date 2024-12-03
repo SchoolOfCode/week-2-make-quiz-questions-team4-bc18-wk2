d63efa1cdd5c4d3b3c5f78728ffdd5a9234efa3f
pepland <136370802+pepland@users.noreply.github.com>
2024-12-03 13:32:42 +0000

    Added Question 1

--- questions.js
+++ questions.js
@@ -1,7 +1,17 @@
 // questions.js
 
 const questions = [
-  // Add your questions here
+  let Question_1 = {
+    question_text: "What was the first hackathon challenge?",
+    question_type: "multiple_choice",
+    options: ["Escape Room", "Calculator", "Spreadsheet", "Romeo and Juliet opera"],
+    correct_option: 0,
+    correct_explanation: "Romeo and Juliet was just an exercise. The hackathon was about an Escape Room"
+  },
+  let Question 2 = {
+
+  },
+  
 ];
 
 // Don't worry about this, we're just exporting the questions
