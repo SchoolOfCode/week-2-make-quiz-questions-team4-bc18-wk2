3be831a4e929b4385f1361f49d51868d38e00425
ash <menicce4@gmail.com>
2024-12-03 13:42:46 +0000

    added second question

--- questions.js
+++ questions.js
@@ -1,18 +1,21 @@
 // questions.js
 
 const questions = [
-  let Question_1 = {
+   {
     question_text: "What was the first hackathon challenge?",
     question_type: "multiple_choice",
     options: ["Escape Room", "Calculator", "Spreadsheet", "Romeo and Juliet opera"],
     correct_option: 0,
     correct_explanation: "Romeo and Juliet was just an exercise. The hackathon was about an Escape Room"
   },
-  let Question 2 = {
-
-  },
-  
-];
+  {
+    question_text: "Which of these is not a room in Disney ideation?",
+    question_type: "multiple_choice",
+    options: ["Dreamer", "Realist", "Critic", "objectivist"],
+    correct_option: 3,
+    correct_explanation: "Dream is coming up with an impossible idea, realist is taking it down an achivable level, and critic is seeing flaws and preventing overscope. Where as objectivist is not part of this"
+  }
+]
 
 // Don't worry about this, we're just exporting the questions
 module.exports = questions;
